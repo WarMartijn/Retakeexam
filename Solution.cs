104555d4d9c718a949becd5722f5a7ad9b572051
WarMartijn <1058740@hr.nl>
2024-01-22 13:23:00 +0100

    Message

--- Solution.cs
+++ Solution.cs
@@ -17,7 +17,7 @@ class Solution{
         var query = (from t in db.Tickets 
                     join b in db.BoardingPasses on t.Id equals b.TicketID
                     where t.Name == person 
-                    select b);
+                    select new BoardingPassWithName(b.FlightID, b.FlightID, b.Fare, b.SeatNumber, t.name, b.IssueTime));
         return query;  //this line of code should be changed 
         
     }
